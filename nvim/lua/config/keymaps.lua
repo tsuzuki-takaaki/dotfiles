0b0a375d6150b40ca5bce7595c3aa6fd64cadc1e
tsuzuki-takaaki <taka.110706@icloud.com>
2024-02-17 19:19:54 +0900

    Add lazy.nvim and replace lua

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+vim.keymap.set('i', '<C-e>', '<Esc>$a')
+vim.keymap.set('i', '<C-a>', '<Esc>^i')
+vim.keymap.set('i', '<C-b>', '<Esc>ha')
+vim.keymap.set('i', '<C-f>', '<Esc>la')
+
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.number = true
+local tabwidth = 2
+vim.opt.tabstop = tabwidth
+vim.opt.softtabstop = tabwidth
+vim.opt.shiftwidth = tabwidth
+vim.opt.clipboard = "unnamedplus"
+vim.opt.hlsearch = true
+vim.opt.syntax = "on"
+
